24e15d4e71306001a92cced923fffc945370c3cb
aaronweggemans <aaronweggemans@hotmail.nl>
2024-04-29 23:06:01 +0200

    contain added relationships for fuel stations

--- src/main/java/com/aaronweggemans/mapadvisorapi/FuelStation/FuelStation.java
+++ src/main/java/com/aaronweggemans/mapadvisorapi/FuelStation/FuelStation.java
@@ -1,10 +1,14 @@
 package com.aaronweggemans.mapadvisorapi.FuelStation;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.aaronweggemans.mapadvisorapi.Coordinate.Coordinate;
+import com.aaronweggemans.mapadvisorapi.FuelStationPrice.FuelStationPrice;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "fuelstations")
@@ -21,5 +25,11 @@ public class FuelStation {
     private String street;
     private String postal_code;
     private String city;
-    private Float coordinate;
+
+    @ManyToOne(cascade = { CascadeType.ALL })
+    @JoinColumn(name = "coordinate")
+    private Coordinate coordinate;
+
+    @OneToMany(mappedBy = "station")
+    private List<FuelStationPrice> prices;
 }

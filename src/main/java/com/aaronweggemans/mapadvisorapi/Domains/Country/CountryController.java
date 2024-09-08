6ede1b30211352c0de813c77708d24537f797c57
aaronweggemans <aaronweggemans@hotmail.nl>
2024-09-08 16:43:42 +0200

    Correct configured endpoints

--- src/main/java/com/aaronweggemans/mapadvisorapi/Domains/Country/CountryController.java
+++ src/main/java/com/aaronweggemans/mapadvisorapi/Domains/Country/CountryController.java
@@ -2,6 +2,7 @@ package com.aaronweggemans.mapadvisorapi.Domains.Country;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,15 @@ public class CountryController {
     @GetMapping
     public List<Country> getCountries() {
         return countryService.getCountries();
+    }
+
+    @GetMapping("random")
+    public Country getRandomCountry() {
+        return countryService.getRandomCountry();
+    }
+
+    @GetMapping("find/{id}")
+    public Country findCountry(@PathVariable Long id) {
+        return countryService.findCountry(id);
     }
 }
--- src/main/java/com/aaronweggemans/mapadvisorapi/Domains/Country/CountryService.java
+++ src/main/java/com/aaronweggemans/mapadvisorapi/Domains/Country/CountryService.java
@@ -1,20 +1,44 @@
 package com.aaronweggemans.mapadvisorapi.Domains.Country;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class CountryService {
     private final CountryRepository countryRepository;
+    private final EntityManager entityManager;
 
     @Autowired
-    public CountryService(CountryRepository countryRepository) {
+    public CountryService(CountryRepository countryRepository, EntityManager entityManager) {
         this.countryRepository = countryRepository;
+        this.entityManager = entityManager;
     }
 
     public List<Country> getCountries() {
         return countryRepository.findAll();
+    }
+
+    public Country getRandomCountry() {
+        long count = ((Number) entityManager.createNativeQuery("select count(*) from countries")
+                .getSingleResult()).longValue();
+
+        // Check could be removed
+        if (count == 0) {
+            throw new IllegalStateException("No countries found in the database.");
+        }
+
+        return (Country) entityManager.createQuery("select c from Country c")
+                .setFirstResult(new Random().nextInt((int) count))
+                .setMaxResults(1)
+                .getSingleResult();
+    }
+
+    public Country findCountry(Long id) {
+        return countryRepository.findById(id).orElse(null);
     }
 }
